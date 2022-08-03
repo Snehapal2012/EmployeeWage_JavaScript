ee74a3990778b81911c56ca36b991a6f873d9f5e
snehapal2012 <snehapal2012@gmail.com>
2022-08-03 19:11:04 +0530

    Modified: write function for daily working hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,17 +1,24 @@
-/**Ability to Calculate Daily Employee Wage based on part time or full time work */
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
+/**Refactor the Code to write a function to get work hours */
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
+function getWorkingHours(empCheck){
+switch(empCheck){
+    case 0: console.log("Employee worked as part-time employee\n");
+    empHrs=PART_TIME_HOURS;
+    return PART_TIME_HOURS;
+
+    case 1: console.log("Employee worked as full-time employee\n");
+    empHrs=FULL_TIME_HOURS;
+    return FULL_TIME_HOURS;
+
+    default: console.log("Employee is absent");
+    empHrs=0;
+    return 0;
+    }
+}
 let empHrs=0;
 empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case IS_PART_TIME: empHrs=PART_TIME_HOURS;
-    break;
-    case IS_FULL_TIME: empHrs=FULL_TIME_HOURS;
-    break;
-    default: empHrs=0;
-}
+empHrs=getWorkingHours(empCheck);
 let empWage= empHrs*WAGE_PER_HOUR;
 console.log("Employee Wage: "+empWage);
