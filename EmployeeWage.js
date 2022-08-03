332db334dfed9400a8e6c66f1625b62395c80628
snehapal2012 <snehapal2012@gmail.com>
2022-08-03 19:29:23 +0530

    Modified: Calculate employee wage for month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,24 +1,27 @@
-/**Refactor the Code to write a function to get work hours */
+/**Calculating Wages for a Month assuming 20 Working Days in a Month*/
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
+const NUM_OF_WORKING_DAYS=20;
 function getWorkingHours(empCheck){
 switch(empCheck){
-    case 0: console.log("Employee worked as part-time employee\n");
+    case 0: console.log("Employee worked as part-time employee\nWorking hours: "+PART_TIME_HOURS);
     empHrs=PART_TIME_HOURS;
     return PART_TIME_HOURS;
-
-    case 1: console.log("Employee worked as full-time employee\n");
+    break;
+    case 1: console.log("Employee worked as full-time employee\nWorking hours: "+FULL_TIME_HOURS);
     empHrs=FULL_TIME_HOURS;
     return FULL_TIME_HOURS;
-
-    default: console.log("Employee is absent");
+    break;
+    default: console.log("Employee is absent\nWorking hours: 0");
     empHrs=0;
     return 0;
     }
 }
 let empHrs=0;
-empCheck=Math.floor(Math.random()*10)%3;
-empHrs=getWorkingHours(empCheck);
+for( let day=0;day<NUM_OF_WORKING_DAYS; day++){
+    let empCheck=Math.floor(Math.random()*10)%3;
+    empHrs+=getWorkingHours(empCheck);
+}
 let empWage= empHrs*WAGE_PER_HOUR;
-console.log("Employee Wage: "+empWage);
+console.log("Total hours: "+empHrs+" Employee Wage: "+empWage);
